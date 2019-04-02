c60a69c68d351448802bd2fc226afe1feda54a53
Tania Medina <tanivelisse@gmail.com>
2019-04-02 15:17:14 -0500

    time interval added

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ console.log("tamagotchi");
 class Tamagotchi {
 	constructor(name){
 		this.name = name;
-		this.age = 0;
+		this.age = null;
 		this.sleepiness = 1;//The Tamagotchi wakes up rested (starts rested), but it's hungry (hunger goes up when it sleeps)
 		this.hunger = 5;//The first thing the Tamagotchi needs is to eat, eating will give it energy to play(the boredome goes up)
 		this.boredom = 1;//The Tamagotchi wants to play after eating (boredom goes down) and then the sleepiness will go up
@@ -29,28 +29,43 @@ class Tamagotchi {
 }
 
 const game = {
+	gameTime: -1,
 	tama: '',
 	lights: true,
 	interval: '',//Pending set up
 	createTama() {
-		nameGiven = $('#input-box').val()
+		nameGiven = $('#input-box').val();
 		console.log(nameGiven);
-		const babyT = new Tamagotchi(nameGiven)	
+		const babyT = new Tamagotchi(nameGiven);	
 		game.tama = babyT
 		//console.log(game.tama.);
 		$('#start-btn').css('display','none');
-		$('#input-box').val('')
+		$('#input-box').val('');
 		$('.name-input').css('display','none');
 		$('#tama-says').append(`Hi I'm ${nameGiven}!`).css('display','flex');
-		$("#metrics").append(`Sleep ${game.tama.sleepiness} Hunger ${game.tama.hunger} Boredom ${game.tama.boredom}`)		
-		}
+		$("#metrics").append(`Sleep ${game.tama.sleepiness} | Hunger ${game.tama.hunger} | Boredom ${game.tama.boredom}`);		
+	},
+		// set time/ age
+	startTime() {
+		console.log("time started");
+		this.gameTime = setInterval( () => {
+			console.log("time is running");
+			this.gameTime++;
+			$('#timer').text(`Clock: ${this.gameTime}`);
+		} , 1000)
+		
+	},
+
+
+  
 	// turnLightOff () {
 	// 	console.log("lights off");
 	// 	//turns lights off
 	// 	//makes tamagotchi go to sleep
 	// 	sleep();
-	
+	//	},
 }
+
 
 
 
@@ -62,6 +77,7 @@ const game = {
 $('#start-btn').on('click', () => {
 	console.log("button works");
 	$('.name-input').css('display','flex');
+	$('#start-btn').css('display', 'none');
 });
 
 //Create event listener for the form button 
@@ -70,12 +86,14 @@ $('#start-btn').on('click', () => {
 $('#assign-name').on('click', (e) => {
 	e.preventDefault();
 	console.log("name assigned");
+	$('#timer').css('display', 'flex')
 	$('#tama1').css('display', 'flex')
-	$('.game-btns button').css('display', 'flex');
+	$('.game-btns button').css('display', 'inline-block');
 	game.createTama();
+	game.startTime();
 });
 
-//display metrics
+
 
 
 
